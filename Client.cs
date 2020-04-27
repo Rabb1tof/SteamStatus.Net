d837d03ec99d41e059e54834748d062422c09f69
Artyom 'Rabb1t' Panin <temka.dust20@gmail.com>
2020-04-28 01:07:45 +0300

    upload last version and removes trash-files

--- Client.cs
+++ Client.cs
@@ -6,7 +6,6 @@ using Newtonsoft.Json;
 
 namespace SteamStatus.Net
 {
-
     public class Client
     {
         public async Task<Json> GetAsync()
@@ -16,8 +15,6 @@ namespace SteamStatus.Net
                 client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Microsoft.CSharp", "1.0"));
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls |
                                                        SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-
-                
                 return JsonConvert.DeserializeObject<Json>(
                     await client.GetStringAsync("https://crowbar.steamstat.us/gravity.json"));
             }
